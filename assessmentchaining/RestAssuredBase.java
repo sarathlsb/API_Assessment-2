e2716eed7c3bb72f536cfe1ff4a7579b9f329a47
sarathlsb <112622526+sarathlsb@users.noreply.github.com>
2022-11-09 09:42:48 +0530

    Update RestAssuredBase.java

--- assessmentchaining/RestAssuredBase.java
+++ assessmentchaining/RestAssuredBase.java
@@ -5,7 +5,8 @@ import io.restassured.RestAssured;
 public class RestAssuredBase {
 	
 public static String sys_id;	
-	
+
+@BeforeMethod
 public void setup() 
 {
 	RestAssured.baseURI="https://dev136537.service-now.com/api/now/table";
